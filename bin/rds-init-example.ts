9ca27ae1c9450c68e41e698a229e28a17d7c8166
debojitr <debojitr@amazon.com>
2023-08-19 17:27:26 +0530

    Added cdk scripts in package.json and renamed the stack

--- bin/rds-init-example.ts
+++ bin/rds-init-example.ts
@@ -9,4 +9,4 @@ import { RdsInitStackExample } from '../demos/rds-init-example'
 const app = new cdk.App()
 
 /* eslint no-new: 0 */
-new RdsInitStackExample(app, 'RdsInitExample')
+new RdsInitStackExample(app, 'RdsWithSeedingScripts')
